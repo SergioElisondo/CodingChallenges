de7c5f2c95ca8c56723c71bc3fd371729f9de0e4
Sergio <sergioelisondo@gmail.com>
2021-02-14 22:20:06 -0800

    codewars 8kyu solutions from my practice

---
+++ String Repeat.js
@@ -0,0 +1,12 @@
+// function repeatStr (number, string) {
+//     return string.repeat(number);
+//   }
+
+// repeatStr(2, "ha ")
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+repeatStr(2, "ha ")
+"ha ha "
