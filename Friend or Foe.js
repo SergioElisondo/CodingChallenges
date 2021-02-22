92b3721cfbf0b411b5ddbe092645f72ca5d2d2be
Sergio <sergioelisondo@gmail.com>
2021-02-21 23:23:37 -0800

    Friend of Foe

---
+++ Friend or Foe.js
@@ -0,0 +1,12 @@
+function friend(friends){
+ let output = []
+ for (i = 0; i < friends.length; i++){
+     if(friends[i].length === 4 ){
+         output.push(friends[i])
+
+     }
+ }
+ return output
+}
+
+#100Devs - leonnoel.com/twitch
