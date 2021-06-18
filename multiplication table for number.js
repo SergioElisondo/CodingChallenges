69e0ec1039ff2c7becc1d419c4d127f8feabf17f
Sergio Elisondo <sergioelisondo@gmail.com>
2021-06-17 17:04:29 -0700

    multiplication table for number.js codewars solution

---
+++ multiplication table for number.js
@@ -0,0 +1,22 @@
+function multiTable(number) {
+    return `1 * ${number} = ${1 * number}
+2 * ${number} = ${2 * number}
+3 * ${number} = ${3 * number}
+4 * ${number} = ${4 * number}
+5 * ${number} = ${5 * number}
+6 * ${number} = ${6 * number}
+7 * ${number} = ${7 * number}
+8 * ${number} = ${8 * number}
+9 * ${number} = ${9 * number}
+10 * ${number} = ${10 * number}`
+}
+
+// or you can write a for loop
+
+const multiTable = (number) => {
+    const numberTable = []
+    for (let i = 1; i <= 10; i++) {
+        numberTable.push(`${i} * ${number} = ${i * number}`)
+    }
+    return numberTable.join('\n')
+}
