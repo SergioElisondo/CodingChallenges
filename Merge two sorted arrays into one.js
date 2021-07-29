871a790023c3953322a669292481473fb2c36703
Sergio Elisondo <sergioelisondo@gmail.com>
2021-07-29 12:35:50 -0700

    Merge Two Sorted Arrays Into One codewars solution 8kyu

--- Merge two sorted arrays into one.js
+++ Merge two sorted arrays into one.js
@@ -6,3 +6,15 @@ return combined
 // refactored with a different concatenation
 
 const mergeArrays = (arr1, arr2) => arr1.concat(arr2).sort((a,b) => a-b).filter((el, i, arr) => arr.indexOf(el) === i)
+
+// using Array.from() and Set()
+
+function mergeArrays(arr1, arr2) {
+  return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+}
+
+// using ... 
+
+function mergeArrays(arr1, arr2) {
+  return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b );
+}
